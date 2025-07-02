fe4b5a28edb9e34837b577056f88c6a51c494cae
YoungJae Ryu <zeroexit@gmail.com>
2025-07-02 16:11:46 +0900

    [Feature] SimilarityChecker

--- SimilarityChecker.cpp
+++ SimilarityChecker.cpp
@@ -1,4 +1,23 @@
-class SimiarityChecker {
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
 
+class SimilarityChecker {
+public:
+	int getSimilarity(const std::string& a, const std::string& b) {
+		if (a.length() == b.length()) return 60;
+ 		if (a.length() >= b.length() * 2) return 0;
+		if (a.length() * 2 <= b.length()) return 0;
+
+		int shortStringLength = -1;
+		if (a.length() > b.length()) shortStringLength = b.length();
+		else shortStringLength = a.length();
+
+		int gap = ::abs((int)(a.length() - b.length()));		
+		int score = static_cast<int>((1.0 - (static_cast<double>(gap) / static_cast<double>(shortStringLength))) * 60.0);
+		return score;
+	}
 };
 
--- main.cpp
+++ main.cpp
@@ -3,7 +3,34 @@
 
 TEST(TS1, TC1)
 {
-	EXPECT_EQ(1, 1);
+	SimilarityChecker sc;
+	int actual = sc.getSimilarity("ASD", "DSA");
+	
+	EXPECT_EQ(60, actual);
+}
+
+TEST(TS1, TC2)
+{
+	SimilarityChecker sc;
+	int actual = sc.getSimilarity("A", "BB");
+
+	EXPECT_EQ(0, actual);
+}
+
+TEST(TS1, TC3)
+{
+	SimilarityChecker sc;
+	int actual = sc.getSimilarity("AAABB", "BAA");
+
+	EXPECT_EQ(20, actual);
+}
+
+TEST(TS1, TC4)
+{
+	SimilarityChecker sc;
+	int actual = sc.getSimilarity("AA", "AAE");
+
+	EXPECT_EQ(30, actual);
 }
 
 int main() {
